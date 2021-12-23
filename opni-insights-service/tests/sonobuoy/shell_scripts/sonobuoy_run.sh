3a097409af66e7eb4b65e05cb984c6edf8241502
jamesonmcg <jameson.mcghee1@gmail.com>
2021-12-23 10:03:10 -0500

    Added Sonobuoy Setup Files
    The files needed to run Sonobuoy

---
+++ opni-insights-service/tests/sonobuoy/shell_scripts/sonobuoy_run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# sonobuoy_run.sh
+# assume Opni and log adapters are already installed
+
+sonobuoy run \
+--kubeconfig ~/.kube/config \
+--namespace "opni-sono" \
+--plugin https://raw.githubusercontent.com/jameson-mcghee/opni-insights-service/insights-int-tests-jrm/opni-insights-service/tests/sonobuoy/opnisono-plugin.yaml
+for i in {1..60}; do sonobuoy retrieve -n opni-sono && break || sleep 5; done
+sonobuoy status -n opni-sono
+sonobuoy delete --wait
+kubectl delete namespace opni-sono
